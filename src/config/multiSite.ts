89b1a760d333b538f53dc7a1826fa0f2254bb327
Eduard Beller <eduard.biellier@justrelate.com>
2023-12-20 16:40:51 +0100

    Copy helpers from current app for the multisite config

---
+++ src/config/multiSite.ts
@@ -0,0 +1,81 @@
+import * as Scrivito from 'scrivito'
+import { Homepage } from '../Objs/Homepage/HomepageObjClass'
+
+const NATIVE_SITE_ID = '6f5a41ce2735d33e'
+const origin = import.meta.env.SCRIVITO_ORIGIN || window.location.origin
+type HomepageInstance = InstanceType<typeof Homepage>
+
+export function baseUrlForSite(siteId: string) {
+  const siteRoot = Scrivito.Obj.onSite(siteId).root() as HomepageInstance | null
+  if (!siteRoot) return
+  const links = siteRoot.get('baseUrl')
+  const primaryBaseUrlForSite = links[0]?.url()
+
+  return primaryBaseUrlForSite || undefined
+}
+
+export function siteForUrl(url: string) {
+  const exactMatchSiteId = bestSiteIdForUrl(
+    url,
+    Scrivito.Obj.onAllSites()
+      .where('_path', 'equals', '/')
+      .toArray() as HomepageInstance[],
+  )
+
+  const nativeSite = Scrivito.Obj.onSite(NATIVE_SITE_ID).root()
+  const versionOfNativeSiteId =
+    url.startsWith(origin) &&
+    bestSiteIdForUrl(
+      url,
+      (nativeSite?.versionsOnAllSites() || []) as HomepageInstance[],
+      pathOf,
+    )
+
+  const siteId = exactMatchSiteId || versionOfNativeSiteId
+  console.log('exactMatchSiteId:', exactMatchSiteId)
+  console.log('versionOfNativeSiteId:', versionOfNativeSiteId)
+
+  if (siteId) {
+    return {
+      siteId,
+      baseUrl: `${originOf(url)}/${pathOf(baseUrlForSite(siteId))}`,
+    }
+  }
+}
+
+function bestSiteIdForUrl(
+  url: string,
+  sites: HomepageInstance[],
+  transformUrl = (s: string) => s,
+) {
+  return sites
+    .sort(
+      (a, b) =>
+        (b.get('baseUrl')[0]?.url()?.length || 0) -
+        (a.get('baseUrl')[0]?.url()?.length || 0),
+    )
+    .find((root) =>
+      root.get('baseUrl').find((link) => {
+        const linkUrl = link.url()
+        if (linkUrl === null) return false
+        return startsWith(transformUrl(url), transformUrl(linkUrl))
+      }),
+    )
+    ?.siteId()
+}
+
+function pathOf(url?: string) {
+  if (!url) return ''
+  return url.replace(/.*?\b(\/|$)/, '')
+}
+
+function originOf(url: string) {
+  return url.replace(/\b\/.*/, '')
+}
+
+function startsWith(url: string, prefix: string) {
+  return (
+    prefix === '' ||
+    !!url.match(new RegExp(`^${prefix.replace(/\W/g, '\\$&')}(\\?|/|$)`))
+  )
+}
